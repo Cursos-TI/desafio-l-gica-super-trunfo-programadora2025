27ceab2b785c873db186ab9e24b2e1d31a7139f9
programadora2025 <sabrina_c_freitas@hotmail.com>
2025-06-04 20:59:45 +0000

    Tentativa às cegas

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -3,46 +3,27 @@
         printf("Desafio Super Trunfo - Países.\n");
         printf("Comparação das Cartas\n");
         printf("\033[1mby Programadora2025\033[0m\n");
- 
- 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
-
-int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
-
     
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+        float densidade1, PIB1;
+        float densidade2, PIB2;
+        
+     printf("digite a densidade:  \n");
+    scanf("%f" &densidade1)
+    scanf("&f" &densidade2)
+    printf ("entre com o PIB:  \n");
+    scanf("%f" &PIB1);
+    scanf("%f" &PIB2);
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
-
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
-
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
-
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
-
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+    if(densidade1 > densidade2){
+        printf("O Pais 1 venceu");
+        {elese}
+        printf("O Pais 2 venceu")
+    }
+    if(PIB1 > PIB2){
+        printf("O Pais 1 venceu");
+        {else}
+        printf(" O Pais 2 Venceu");
+    }
 
  
         return 0;
